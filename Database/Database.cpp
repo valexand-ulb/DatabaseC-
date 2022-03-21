b8fa4ccd9d18dd61d8c0b9cad7ff6267bad78890
alex <vinovrski.alexandre@ulb.be>
2022-03-21 09:09:44 +0100

    CmakeList et autre

--- Database/Database.cpp
+++ Database/Database.cpp
@@ -1,5 +1,44 @@
 #include "Database.h"
 
+Database::Database() {
+    sqlite3_open("./test.db", &this->DB);
+    char* messageError;
+
+    std::string sql1 = "CREATE TABLE IF NOT EXISTS UserEntry("
+                       "username        TEXT,"
+                       "password        TEXT,"
+                       "PRIMARY KEY (username))";
+    sqlite3_exec(this->DB, sql1.c_str(), NULL, 0, &messageError);
+
+    std::string sql2 = "CREATE TABLE IF NOT EXISTS ScoreEntry ("
+                       "username           TEXT,"
+                       "nameOfLevel       TEXT,"
+                       "score             INTEGER,"
+                       "PRIMARY KEY (username, nameOfLevel)"
+                       "FOREIGN KEY (username) REFERENCES UserEntry(username))";
+    sqlite3_exec(this->DB, sql2.c_str(), NULL, 0, &messageError);
+
+    std::string sql3 = "CREATE TABLE IF NOT EXISTS FriendshipEntry ("
+                       "username1	     TEXT,"
+                       "username2	     TEXT,"
+                       "PRIMARY KEY (username1, username2)"
+                       "FOREIGN KEY (username1) REFERENCES UserEntry(username),"
+                       "FOREIGN KEY (username2) REFERENCES UserEntry(username))";
+    sqlite3_exec(this->DB, sql3.c_str(), NULL, 0, &messageError);
+
+    std::string sql4 = "CREATE TABLE IF NOT EXISTS CustomizedLevel("
+                       "creatorUsername      TEXT,"
+                       "nameOfLevel          TEXT,"
+                       "listOfEnnemies       TEXT,"
+                       "listOfPositions      TEXT,"
+                       "listOfSpawnTime      TEXT,"
+                       "numberOfLikes        INT,"
+                       "nameOfLikers         TEXT,"
+                       "PRIMARY KEY (nameOfLevel)"
+                       "FOREIGN KEY (creatorUsername) REFERENCES UserEntry(username))";
+    sqlite3_exec(this->DB, sql4.c_str(), NULL, 0, &messageError);
+}
+
 int main(){
     return 0;
 }
--- Database/Database.h
+++ Database/Database.h
@@ -1,9 +1,18 @@
 #ifndef DATABASEC_DATABASE_H
 #define DATABASEC_DATABASE_H
 
+#include <iostream>
+#include <stdio.h>
+#include <sqlite3.h>
+#include <vector>
+#include <string>
 
 class Database {
-
+private:
+    sqlite3* DB;
+public:
+    Database();
+    ~Database();
 };
 
 
